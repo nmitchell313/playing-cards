9d97395d185030e74c218903db9c5cb9dbd1828a
nmitchell313 <nmitchel11@cnm.edu>
2021-10-11 15:51:53 -0600

    gitignore

--- src/main/java/edu/cnm/deepdive/Test.java
+++ src/main/java/edu/cnm/deepdive/Test.java
@@ -3,21 +3,26 @@ package edu.cnm.deepdive;
 import edu.cnm.deepdive.model.Card;
 import edu.cnm.deepdive.model.Rank;
 import edu.cnm.deepdive.model.Suit;
+
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Test {
 
     public static void main(String[] args) {
         List<Card> deck = new ArrayList<>();
-        for (Suit s: Suit.values()){
-            for (Rank r: Rank.values()) {
+        for (Suit s : Suit.values()) {
+            for (Rank r : Rank.values()) {
                 Card c = new Card(r, s);
                 deck.add(c);
             }
         }
 
+        System.out.println(deck);
+        Collections.shuffle(deck, new SecureRandom());
+        System.out.println(deck);
+
     }
-
-
 }
